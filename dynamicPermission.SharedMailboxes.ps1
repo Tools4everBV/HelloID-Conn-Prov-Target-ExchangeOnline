065c6e26a8bcb12959adfa7ed15c6a200d848e5f
rschouten97 <69046642+rschouten97@users.noreply.github.com>
2023-09-06 10:36:42 +0200

    hotfix: correct UseBasicParsing $true

--- dynamicPermission.SharedMailboxes.ps1
+++ dynamicPermission.SharedMailboxes.ps1
@@ -184,7 +184,7 @@ try {
             resource      = "https://outlook.office365.com"
         }
     
-        $Response = Invoke-RestMethod -Method POST -Uri $authUri -Body $body -ContentType "application/x-www-form-urlencoded"
+        $Response = Invoke-RestMethod -Method POST -Uri $authUri -Body $body -ContentType "application/x-www-form-urlencoded" -UseBasicParsing:$true
         $accessToken = $Response.access_token
 
         # Connect to Exchange Online in an unattended scripting scenario using an access token.
