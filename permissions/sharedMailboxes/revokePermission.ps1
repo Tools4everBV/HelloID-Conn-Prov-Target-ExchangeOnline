d9cdb80aa31279dfac81c8c5fdce69c7f2e61678
rschouten97 <69046642+rschouten97@users.noreply.github.com>
2024-06-06 17:36:04 +0200

    removed testing code

--- permissions/sharedMailboxes/revokePermission.ps1
+++ permissions/sharedMailboxes/revokePermission.ps1
@@ -4,7 +4,7 @@
 # Revoke shared mailbox permission (full access, send as or send on behalf) from account
 # PowerShell V2
 #####################################################
-$actionContext.DryRun = $false
+
 # Enable TLS1.2
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
 
